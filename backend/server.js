ce0e1ef6baba82940c3fa2685afe1b7dbe89677b
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-07-17 15:50:06 -0700

    Creating a exercise tracking application using MERN

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// body-parser is included with express
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// connfigures enviorment variables so that there can be .env files
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// uri helps establish a connection to my cluster in mongodb
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Connection to the MongoDB database has been established succesfully');
+})
+
+app.listen(port, () => {
+    console.log(`The Server is running on port: ${port}`);
+});
+
+
