2d4ae54c5190c1eeafb2a61e1ee5b64419a31dd8
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-07-17 16:09:19 -0700

    Created get and post requests for users. / (root) and /add (new user)

---
+++ backend/routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+// requiring the mongoose model
+let User = require('../models/user.model');
+
+// creating the first endpoint that handles http get requests for root url
+router.route('/').get((req, res) =>{
+    // .find is a mongoose method that finds a list of all users from the db. Returns a promise
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('ERROR: ' + err));
+});
+
+// adds a new user by username
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('A new user has been added'))
+    .catch(err => res.status(400).json('ERROR: ' + err));
+});
+
+module.exports = router;
