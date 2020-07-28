c43e32c3e2a2b811f04e425cefda5f892c3c00eb
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-07-28 10:25:42 -0700

    Added routes in App.js and a navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navbar from "./components/navbar.component";
-import ExercisesList from "./components/exercises-list.component";
-import CreateExercise from "./components/create-exercise.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateUser from './components/create-user.component';
+import Navbar from "./components/navbar.component.js";
+import ExercisesList from "./components/exercises-list.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import EditExercise from "./components/edit-exercise.component.js";
+import CreateUser from './components/create-user.component.js';
 
 
 
